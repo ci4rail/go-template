f20d3a225d1002ef25232784ea69d73892182b40
batthebee <florian.reinhold@posteo.de>
2024-08-22 16:40:41 +0200

    fix: release

--- cmd/go-template/main_test.go
+++ cmd/go-template/main_test.go
@@ -7,6 +7,8 @@ import (
 func TestGetSensorValue(t *testing.T) {
 	expected := exampleSensorValue
 	actual := getSensorValue("sensor1")
+
+	t.Errorf("expected %v, but got %v", expected, actual)
 	if actual != expected {
 		t.Errorf("expected %v, but got %v", expected, actual)
 	}
