aea4ce13dea25a648a20474aaf1a94b3d2d7ff47
batthebee <florian.reinhold@posteo.de>
2024-09-11 14:25:53 +0200

    fix: precommit

--- cmd/go-template/main_test.go
+++ cmd/go-template/main_test.go
@@ -8,7 +8,6 @@ func TestGetSensorValue(t *testing.T) {
 	expected := exampleSensorValue
 	actual := getSensorValue("sensor1")
 
-	t.Errorf("expected %v, but got %v", expected, actual)
 	if actual != expected {
 		t.Errorf("expected %v, but got %v", expected, actual)
 	}
